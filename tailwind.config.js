3c8ee9df5252f486dde49c53ad50a107dc2cbc5f
Nilu <nilu.shahmoradi@gmail.com>
2021-07-09 18:55:20 +0200

    Beispiel: Eine eigeneFarbpalette umsetzen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -5,18 +7,30 @@ module.exports = {
     debugScreens: {
       position: ['top', 'left'],
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      black: colors.black,
+      blue: colors.blue,
+      yellow: colors.yellow,
+      blueGray: colors.blueGray,
+      cyan: colors.cyan,
+      lime: colors.lime,
+      roseRed: colors.rose,
+    },
     extend: {},
   },
   variants: {
     extend: {
 
-      colors: {
+      /* colors: {
         darkBlue: '#212f49'
       },
 
       spacing: {
         13: '3.25rem',
-      },
+      }, */
 
     },
   },
